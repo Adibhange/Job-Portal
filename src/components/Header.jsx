f2f551b8d74e00c9abb6f1a6ec99f012c64f38db
Adibhange <bhangeaditya13@gmail.com>
2024-09-02 13:42:36 +0530

    Add logo and login button in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
 const Header = () => {
-	return (
-		<header className='container'>
-			Header <p></p>
-		</header>
-	);
+  return (
+    <header className="container">
+      <nav className="flex items-center justify-between py-4">
+        <Link>
+          <img src="/logo.png" alt="Logo" className="h-20" />
+        </Link>
+
+        <Button variant="outline">Login</Button>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
