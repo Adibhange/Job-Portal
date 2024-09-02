172b1eda39249e078b00df83fd3cdcfbbbf31a61
Adibhange <bhangeaditya13@gmail.com>
2024-09-02 12:20:38 +0530

    Create React app with Shadcn UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<div>
+				<h1 className='underline'>Hello World</h1>
+				<Button>Login</Button>
+			</div>
+		</>
+	);
+}
+
+export default App;
