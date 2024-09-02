448c008a51d4ba20b41655ea9c2ce199a9103994
Adibhange <bhangeaditya13@gmail.com>
2024-09-02 20:10:30 +0530

    Create Hero section on Home page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,18 +2,18 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
 const Layout = () => {
-	return (
-		<>
-			<div className='grid-bg'></div>
-			<Header />
-			<main className='container min-h-screen'>
-				<Outlet />
-			</main>
-			<footer className='p-10 text-center bg-gray-800 mt-10'>
-				<p>&copy; 2024 Aditya Bhange. All rights reserved.</p>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <div className="grid-bg"></div>
+      <Header />
+      <main className="container min-h-screen">
+        <Outlet />
+      </main>
+      <footer className="mt-10 bg-gray-800 p-10 text-center">
+        <p>&copy; 2024 Aditya Bhange. All rights reserved.</p>
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,50 @@
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+
 const Home = () => {
-	return (
-		<div>
-			Home <p></p>
-		</div>
-	);
+  return (
+    <section className="flex flex-col gap-10 py-10 sm:gap-20 sm:py-20">
+      {/* Hero Section */}
+      <section className="text-center">
+        <h1 className="gradient-title flex flex-col items-center justify-center text-4xl font-extrabold tracking-tighter sm:text-6xl lg:text-8xl">
+          Find Your Dream Job
+          <span className="flex items-center gap-2 sm:gap-6">
+            and get
+            <img src="/logo.png" alt="Logo" className="h-14 sm:h-24 lg:h-32" />
+          </span>
+        </h1>
+        <p className="text-xs text-gray-300 sm:mt-4 sm:text-xl">
+          Discover thousands of job opportunities or find the ideal candidate
+          with ease
+        </p>
+
+        <div className="mt-10 flex justify-center gap-6">
+          <Link to="/jobs">
+            <Button variant="blue" size="xl">
+              Find Job
+            </Button>
+          </Link>
+          <Link to="/post-job">
+            <Button variant="destructive" size="xl">
+              Post Job
+            </Button>
+          </Link>
+        </div>
+      </section>
+
+      {/* Carousel */}
+      <section></section>
+
+      {/* Banner */}
+      <section></section>
+
+      {/* Card */}
+      <section></section>
+
+      {/* Accordian */}
+      <section></section>
+    </section>
+  );
 };
 
 export default Home;
