ea5554f98a3f5ea99ee230bbe9c1fb626c7485a5
Adibhange <bhangeaditya13@gmail.com>
2024-09-04 13:22:48 +0530

    Create separate section for My Jobs page for candidates and recruiters

--- src/api/saveJobApi.js
+++ src/api/saveJobApi.js
@@ -28,3 +28,17 @@ export async function saveJob(token, { alreadySaved }, saveData) {
     return data;
   }
 }
+
+export async function getSavedJobs(token) {
+  const supabase = await supabaseClient(token);
+  const { data, error } = await supabase
+    .from("saved_jobs")
+    .select("*, job: jobs(*, company: companies(name,logo_url))");
+
+  if (error) {
+    console.error("Error fetching Saved Jobs:", error);
+    return null;
+  }
+
+  return data;
+}
